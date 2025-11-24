6325b786a6001be1faf1b0bbd44a36c7b8e32d91
isalamajor <isabel.hernandez7barrio@gmail.com>
2025-11-24 19:42:52 +0100

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="relative w-full h-full bg-radial animate-radial">      
       
-      <div className="relative z-10 flex flex-col gap-5 justify-center h-full pt-40 px-15 sm:px-20 md:px-25 lg:px-50 xl:px-100">      {/*Intro */}
+      <div className="relative z-10 flex flex-col gap-5 justify-center h-full pt-40 px-10 sm:px-20 md:px-25 lg:px-50 xl:px-100">      {/*Intro */}
         
         <div className="flex flex-row gap-5 mb-10">
           <img className="shadow-2xl rounded-full w-45 h-45" src={"/cv_foto_isa_2.png"}></img>
@@ -14,9 +14,8 @@ function App() {
             <p className="cursor-pointer text-6xl w-100 hover:text-purple-300 transition-colors duration-300">Barrio</p>
           </div>
         </div>
-        <p>I'm a final year <span>Computer Engineering</span> student from <span>Madrid, Spain</span>. Keen on Full-Stack development,
-          software engineering, python and web programming. I am currently working on my
-          thesis project and will graduate at the end of this school year, in July 2026. 
+        <p>
+          I am a final-year <span>Computer Engineering</span> student from <span>Madrid, Spain</span>, with a strong interest in full-stack development, software engineering, Python, and web programming. I am currently working on my thesis project and will graduate at the end of this academic year, in July 2026. 
         </p>
         <Contact/>
 
@@ -24,25 +23,22 @@ function App() {
           
           {/* Studies */}
           <div className='flex flex-col gap-5'>
-            <h1 className='text-center md:text-left'>Studies</h1>
+            <h1 className='text-center md:text-left'>Education</h1>
             <div className="flex flex-row gap-5 justify-start items-center">
                 <img className="rounded-full w-20 h-20 cursor-pointer" src={"/uc3m.png"}
                 onClick={() => window.open('https://www.uc3m.es/bachelor-degree/computer-science', '_blank')}/>
                 <div>
                 <h2 className="text-lg font-semibold">Universidad Carlos III de Madrid</h2>
-                <p>I studied a Computer Engineering degree starting in 2021 and finishing all 
-                  courses succesfully in June 2025. I have left only my thesis project, which I
-                  will be defending next July.
+                <p>
+                  I started my Computer Engineering degree in 2021 and have successfully completed all courses as of June 2025. The only requirement remaining is my thesis project, which I will defend next July.
                 </p>
                 </div>
             </div>
             <div className="flex flex-row gap-5 justify-start text-right items-center">
                 <div>
                 <h2 className="text-lg font-semibold">Politechnika Lubelska</h2>
-                <p>In the spring semester 2023 I had the chance to go study abroad to the Polish
-                  city of Lublin. There, I had such an enrichising experience in which I got to 
-                  know lots of different people and cultures, improve my English skills, and 
-                  I got intereseted for the first time in web programming.
+                <p>
+                  In the spring semester of 2023, I had the opportunity to study abroad in Lublin, Poland. This was an enriching experience that allowed me to meet people from diverse backgrounds, improve my English skills, and discover my interest in web programming.
                 </p>
                 </div>
                 <img className="rounded-full w-20 h-20 cursor-pointer" src={"/pollub.png"}
@@ -55,12 +51,8 @@ function App() {
           <div className='flex flex-col gap-5'>
             <h1 className='text-center md:text-left'>Experience</h1>
             <div className="">
-                <h2 className="text-lg font-semibold">Sofware development intern</h2>
-                <p>From February to June 2025 I worked as an intern in Accenture. My tasks consisted
-                  on delevoping applications and web components in the low-code platform Outsystems 11 and
-                  Outsystems ODC. This experience helped learn how to work in a real team and understand
-                  in depth how React components interact with each other, highlighting children-parent 
-                  relationships.
+                <h2 className="text-lg font-semibold">Sofware Development Intern</h2>
+                <p>From February to June 2025 I worked as a software development intern, focusing on developing applications and web components using the low-code platform OutSystems 11 and OutSystems ODC. This experience taught me how to collaborate effectively in a real team environment and gave me a deeper understanding of how React components interact, particularly parent-child relationships.
                 </p>
             </div>
           </div>
@@ -76,10 +68,8 @@ function App() {
                 />
                 <div>
                   <p className='text-lg font-semibold'>Spotify Sharer</p>
-                  <p> This app is made to share Spotify songs with friends. It allows you to upload
-                    a song through just its Spotify sharing link and have one or more lists of shared 
-                    songs, which are stored in a database accessed though an API I built. The app checks
-                    whether the share link entered is valid through Spotify API for Developers.
+                  <p> 
+                    A web app that allows users to share Spotify songs with friends. Users can upload songs using Spotify share links and create one or more lists of shared songs, stored in a database accessed via an API I built. The app validates share links through the Spotify API.
                   </p>
                   <div className='pt-2 flex flex-wrap w-full gap-1'>
                       <TechTag tech='React'/>
@@ -94,10 +84,8 @@ function App() {
             <div className='flex flex-col md:flex-row md:flex-row gap-5'>
                 <div className='order-2 md:order-1'>
                   <p className='text-lg font-semibold'>Letterex</p>
-                  <p> I have been working on this app for some months now. It consists on a web application
-                    built in React that offers a platform, kind of a social network, to allow users to share letters to learn languages
-                    through a collaborative method. I now have a quite complete version, but the final
-                    version will be finished and deployed by April
+                  <p> 
+                    A web application built in React that serves as a platform for users to share letters to learn languages collaboratively. The app is in an advanced stage, with the final version planned for deployment by April.
                   </p>
                   <div className='pt-2 flex flex-wrap w-full gap-1'>         
                     <TechTag tech='React'/>
@@ -116,10 +104,9 @@ function App() {
                 <img className='shadow-2xl w-full md:w-100 h-full h-max-100 md:h-70 rounded-lg bg-gray-900 rounded-lg cursor-pointer object-cover' src='/frontend-mugs.png'
                 onClick={() => window.open("https://frontend-mugs-z.vercel.app/", "_blank")}/>
                 <div>
-                <p className='text-lg font-semibold'>Frontend mugs</p>
-                <p>This is the frontend of an online store with several mugs that can be added to
-                  the cart, and then you have a checkout with a form to the shipping data. You can
-                  acces to detailed information of each product in its separate page.
+                <p className='text-lg font-semibold'>Frontend Mugs</p>
+                <p>
+                  Frontend of an online store where users can add mugs to their cart and complete a checkout form for shipping information. Each product has a dedicated page with detailed information.
                 </p>
                 <div className='pt-2 flex flex-wrap w-full gap-1'>  
                     <TechTag tech='CSS'/>
@@ -132,9 +119,8 @@ function App() {
             <div className='flex flex-col md:flex-row gap-5'>
                 <div className='order-2 md:order-1'>
                   <p className='text-lg font-semibold'>Frogs Know</p>
-                  <p>I have a small tiny obsession with frogs, I think they are so cute and interesting; so
-                    I made this site showing different species and characteristics of them. The deployed site is 
-                    an static version of the website I built, which retreived the data from a database through PHP.
+                  <p>
+                    A personal project inspired by my interest in frogs. The website showcases different frog species and their characteristics. The deployed version is static, but the site retrieves data from a database using PHP.
                   </p>
                   <div className='pt-2 flex flex-wrap w-full gap-1'>  
                     <TechTag tech='CSS'/>
