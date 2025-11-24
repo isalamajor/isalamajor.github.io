e00a7396f1d915d99c2c3e6ab20f6203a5f05561
isalamajor <isabel.hernandez7barrio@gmail.com>
2025-11-24 23:45:12 +0100

    fix dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { Github, Linkedin, Mail } from 'lucide-react'
+import { Github, Linkedin, Mail, Terminal, ChevronLeft, ChevronRight } from 'lucide-react'
 
 function App() {
   return (
-    <div className="relative w-full h-full bg-radial animate-radial">      
+    <div className="relative w-screen h-full bg-radial animate-radial">      
       
-      <div className="relative z-10 flex flex-col gap-5 justify-center w-full h-full pt-40 px-10 sm:px-20 md:px-25 lg:px-50 xl:px-100">      
+      <div className="relative z-10 flex flex-col gap-5 justify-center w-full h-full pt-40 px-10 lg:px-20 lg:px-25 lg:px-50 xl:px-85">      
         
         {/* Intro */}
-        <div className="flex flex-row gap-5 mb-10 items-center">
-          <img className="shadow-2xl rounded-full w-35 h-35 md:w-45 md:h-45" src={"/cv_foto_isa_2.png"}></img>
+        <div className="flex flex-row gap-5 mb-10 items-center justify-center sm:justify-start">
+          <img className="shadow-2xl rounded-full w-30 h-30 lg:w-45 lg:h-45" src={"/cv_foto_isa_2.png"}></img>
           <div className='flex flex-col'>
-            <p className="cursor-pointer text-5xl md:text-6xl hover:text-purple-500 transition-colors duration-300">Isabel</p>
-            <p className="cursor-pointer text-5xl md:text-6xl hover:text-purple-400 transition-colors duration-300">Hernández</p>
-            <p className="cursor-pointer text-5xl md:text-6xl hover:text-purple-300 transition-colors duration-300">Barrio</p>
+            <p className="cursor-pointer text-4xl lg:text-6xl hover:text-purple-600 dark:hover:text-purple-500 transition-colors duration-300">Isabel</p>
+            <p className="cursor-pointer text-4xl lg:text-6xl hover:text-purple-500 dark:hover:text-purple-400 transition-colors duration-300">Hernández</p>
+            <p className="cursor-pointer text-4xl lg:text-6xl hover:text-purple-400 dark:hover:text-purple-300 transition-colors duration-300">Barrio</p>
           </div>
         </div>
         <p>
@@ -24,7 +24,7 @@ function App() {
           
           {/* Studies */}
           <div className='flex flex-col gap-5'>
-            <h1 className='text-center md:text-left'>Education</h1>
+            <h1 className='text-center lg:text-left'>&lt;Education&#47;&gt;</h1>
             <div className="flex flex-row gap-5 justify-start items-center">
                 <img className="rounded-full w-20 h-20 cursor-pointer" src={"/uc3m.png"}
                 onClick={() => window.open('https://www.uc3m.es/bachelor-degree/computer-science', '_blank')}/>
@@ -50,7 +50,8 @@ function App() {
 
           {/* Experience */}
           <div className='flex flex-col gap-5'>
-            <h1 className='text-center md:text-left'>Experience</h1>
+            <h1 className='text-center lg:text-left'>&gt;_Experience</h1>
+              
             <div className="">
                 <h2 className="text-lg font-semibold">Sofware Development Intern</h2>
                 <p>From February to June 2025 I worked as a software development intern, focusing on developing applications and web components using the low-code platform OutSystems 11 and OutSystems ODC. This experience taught me how to collaborate effectively in a real team environment and gave me a deeper understanding of how React components interact, particularly parent-child relationships.
@@ -60,12 +61,11 @@ function App() {
 
           {/* Projects */}
           <div className='flex flex-col gap-5'>
-            <h1 className='text-center md:text-left'>Projects</h1>
+            <h1 className='text-center lg:text-left'>&lt;Projects&#47;&gt;</h1>
 
-          
           <div className='flex flex-col gap-10'>
-            <div className='flex flex-col md:flex-row gap-5'>
-              <img className='shadow-2xl w-full md:w-100 h-full h-max-100 md:h-70 rounded-lg bg-gray-900 rounded-lg cursor-pointer object-cover' src='/spotify-sharer.png'
+            <div className='flex flex-col lg:flex-row gap-5'>
+              <img className='shadow-2xl w-full lg:w-100 h-full h-max-100 lg:h-70 rounded-lg bg-gray-900 rounded-lg cursor-pointer object-cover' src='/spotify-sharer.png'
                 />
                 <div>
                   <p className='text-lg font-semibold'>Spotify Sharer</p>
@@ -82,8 +82,8 @@ function App() {
                 </div>
             </div>
 
-            <div className='flex flex-col md:flex-row md:flex-row gap-5'>
-                <div className='order-2 md:order-1'>
+            <div className='flex flex-col lg:flex-row lg:flex-row gap-5'>
+                <div className='order-2 lg:order-1'>
                   <p className='text-lg font-semibold'>Letterex</p>
                   <p> 
                     A web application built in React that serves as a platform for users to share letters to learn languages collaboratively. The app is in an advanced stage, with the final version planned for deployment by April.
@@ -97,12 +97,12 @@ function App() {
                     <TechTag tech='ReactRouter'/>
                   </div>
                 </div>
-                <img className='order-1 md:order-2 shadow-2xl w-full md:w-100 h-full h-max-100 md:h-70 rounded-lg bg-gray-900 rounded-lg cursor-pointer object-cover' src='/letterex.png'
+                <img className='order-1 lg:order-2 shadow-2xl w-full lg:w-100 h-full h-max-100 lg:h-70 rounded-lg bg-gray-900 rounded-lg cursor-pointer object-cover' src='/letterex.png'
                 />
             </div>
 
-            <div className='flex flex-col md:flex-row gap-5'>
-                <img className='shadow-2xl w-full md:w-100 h-full h-max-100 md:h-70 rounded-lg bg-gray-900 rounded-lg cursor-pointer object-cover' src='/frontend-mugs.png'
+            <div className='flex flex-col lg:flex-row gap-5'>
+                <img className='shadow-2xl w-full lg:w-100 h-full h-max-100 lg:h-70 rounded-lg bg-gray-900 rounded-lg cursor-pointer object-cover' src='/frontend-mugs.png'
                 onClick={() => window.open("https://frontend-mugs-z.vercel.app/", "_blank")}/>
                 <div>
                 <p className='text-lg font-semibold'>Frontend Mugs</p>
@@ -117,8 +117,8 @@ function App() {
                 </div>
             </div>
 
-            <div className='flex flex-col md:flex-row gap-5'>
-                <div className='order-2 md:order-1'>
+            <div className='flex flex-col lg:flex-row gap-5'>
+                <div className='order-2 lg:order-1'>
                   <p className='text-lg font-semibold'>Frogs Know</p>
                   <p>
                     A personal project inspired by my interest in frogs. The website showcases different frog species and their characteristics. The deployed version is static, but the site retrieves data from a database using PHP.
@@ -129,7 +129,7 @@ function App() {
                     <TechTag tech='React'/>
                   </div>
                 </div>
-                <img className='order-1 md:order-2 shadow-2xl w-full md:w-100 h-full h-max-100 md:h-70 rounded-lg bg-gray-900 rounded-lg cursor-pointer object-cover' src='/frogsknow.png'
+                <img className='order-1 lg:order-2 shadow-2xl w-full lg:w-100 h-full h-max-100 lg:h-70 rounded-lg bg-gray-900 rounded-lg cursor-pointer object-cover' src='/frogsknow.png'
                 onClick={() => window.open("https://frogs-know.netlify.app/localhost_3000/", "_blank")}/>
             </div>
           </div>
@@ -137,7 +137,7 @@ function App() {
         
         </div>
 
-      <div className='pt-30 pb-5 sm:pb-0'>
+      <div className='pt-30 pb-5 lg:pb-0'>
         <Contact/>
       </div>
       </div>
@@ -156,7 +156,7 @@ const TechTag = ( { tech }: TagProps ) => {
   const techs = ['react', 'javascript', 'nodejs', 'css', 'jwt', 'express', 'mongodb', 'reactrouter']
   if (!techs.includes(tech.toLowerCase())) return <p>NO</p>
   return (
-    <div className='w-fit bg-[#23272F]/70 px-2 py-1 rounded-full flex flex-row gap-1 items-center'>
+    <div className='w-fit bg-[#23272F]/20 dark:bg-[#23272F]/70 px-2 py-1 rounded-full flex flex-row gap-1 items-center'>
       <img src={`/${tech.toLowerCase()}.svg`} className='w-4 h-4'/>
       <p className='text-sm'>{tech}</p>
     </div>
@@ -166,20 +166,20 @@ const TechTag = ( { tech }: TagProps ) => {
 
 const Contact = () => {
   return (
-    <div className='w-full justify-center my-10 flex flex-row gap-2'>
-      <button className="rounded-full p-1 bg-opacity-0 border border-1 border-white w-fit flex flex-row gap-1 items-center"
+    <div className='w-full justify-center my-10 flex flex-wrap gap-2'>
+      <button className="text-xs md:text-md rounded-full p-1 bg-opacity-0 border border-1 border-purple-300 dark:border-white w-fit flex flex-row gap-1 items-center"
       onClick={() => window.open("https://github.com/isalamajor/", "_blank")}>
         <Github/>
-        Github 
+        <p>Github</p>
       </button>
       
-      <button className="rounded-full p-1 bg-opacity-0 border border-1 border-white w-fit flex flex-row gap-1 items-center"
+      <button className="rounded-full bg-opacity-0 border border-1 border-purple-300 dark:border-white w-fit flex flex-row gap-1 items-center"
       onClick={() => window.open("https://www.linkedin.com/in/isabel-hernández-barrio-408a38326", "_blank")}>
         <Linkedin/>
         LinkedIn
       </button>
       
-      <button className="rounded-full p-1 bg-opacity-0 border border-1 border-white w-fit flex flex-row gap-1 items-center"
+      <button className="rounded-full p-1 bg-opacity-0 border border-1 border-purple-300 dark:border-white w-fit flex flex-row gap-1 items-center"
         onClick={() => window.location.href = "mailto:isahbarrio@gmail.com"}>
           <Mail/>
           Contact 
